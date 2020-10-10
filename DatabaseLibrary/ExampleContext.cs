2a958473b0a612fa53efe0150c9343c7577d6f77
MPD97 <mateuszpracadom@gmail.com>
2020-10-10 20:55:15 +0200

    Created ExampleContext

---
+++ DatabaseLibrary/ExampleContext.cs
@@ -0,0 +1,15 @@
+﻿using DatabaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseLibrary
+{
+    public class ExampleContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        public ExampleContext(DbContextOptions<ExampleContext> options) : base(options)
+        {
+        }
+    }
+}
