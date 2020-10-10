246b583ea79261220a3d92f413960822e6a09249
MPD97 <mateuszpracadom@gmail.com>
2020-10-10 21:07:32 +0200

    FluentAPI for OnModelCreating

--- DatabaseLibrary/ExampleContext.cs
+++ DatabaseLibrary/ExampleContext.cs
@@ -11,5 +11,25 @@ namespace DatabaseLibrary
         public ExampleContext(DbContextOptions<ExampleContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().Property(c => c.CountryName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Person>().Property(p => p.FirstName)
+                .HasMaxLength(100)
+                .IsRequired();
+            modelBuilder.Entity<Person>().Property(p => p.SecondName)
+                .HasMaxLength(100)
+                .IsRequired(false);
+            modelBuilder.Entity<Person>().Property(p => p.LastName)
+                .HasMaxLength(150)
+                .IsRequired();
+            
+            base.OnModelCreating(modelBuilder);
+            
+        }
     }
 }
