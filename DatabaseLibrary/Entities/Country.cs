e49483807737e3649b6d5c34d5b575cbd427be23
MPD97 <mateuszpracadom@gmail.com>
2020-10-10 20:29:48 +0200

    Added Person and Country entities

---
+++ DatabaseLibrary/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DatabaseLibrary.Entities
+{
+    public class Country
+    {
+        public short CountryId { get; set; }
+        public string CountryName { get; set; }
+
+        public ICollection<Person> Citizenship { get; set; }
+    }
+}
---
+++ DatabaseLibrary/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DatabaseLibrary.Entities
+{
+    public class Person
+    {
+        public long PersonId { get; set; }
+
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        
+        public Country Nationality { get; set; }
+        public short NationalityId { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Unknown
+    }
+}
